b223c2390027e84e3f99dbb40fabf90187374a33
Grzegorz Sienkiewicz <grzegorz.sienkiewicz@eu.jll.com>
2019-11-18 15:29:33 +0100

    Add server.js, procfile, and other Kodilla sending

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -4,6 +4,37 @@ import OrderSummary from '../OrderSummary/OrderSummary.js';
 import PropTypes from 'prop-types';
 import OrderOption from '../OrderOption/OrderOption.js';
 import Pricing from '../../../data/pricing.json';
+import Button from '../../common/Button/Button';
+import { calculateTotal } from '../../../utils/calculateTotal';
+import { formatPrice } from '../../../utils/formatPrice';
+import settings from '../../../data/settings';
+
+const sendOrder = (options, tripCost) => {
+  const totalCost = formatPrice(calculateTotal(tripCost, options));
+
+  const payload = {
+    ...options,
+    totalCost,
+  };
+
+  const url = settings.db.url + '/' + settings.db.endpoint.orders;
+
+  const fetchOptions = {
+    cache: 'no-cache',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  };
+
+  fetch(url, fetchOptions)
+    .then(function (response) {
+      return response.json();
+    }).then(function (parsedResponse) {
+      console.log('parsedResponse', parsedResponse);
+    });
+};
 
 const OrderForm = (props) => {
   return (
@@ -13,6 +44,7 @@ const OrderForm = (props) => {
           <OrderOption currentValue={props.options[option.id]} setOrderOption={props.setOrderOption} {...option} options={props.options} />
         </Col>
       ))}
+      <Button onClick={() => sendOrder(props.options, props.tripCost)}>Order now!</Button>
       <Col xs={12}>
         <OrderSummary tripCost={props.tripCost} options={props.options} />
       </Col>
